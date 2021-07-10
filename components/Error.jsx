4d48f70e49464ea351d182d4d036a7ab57a67deb
Paul Chávez <paulchavezromero20@gmail.com>
2021-07-10 18:05:48 -0500

    [update] Error added

---
+++ components/Error.jsx
@@ -0,0 +1,16 @@
+
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+
+const Error = ({ message }) => (
+  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+    <p className="font-bold">Error</p>
+    <p>{message}</p>
+  </div>
+)
+
+Error.propTypes = {
+  message: PropTypes.string.isRequired
+}
+
+export default memo(Error)
--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,55 @@
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 import Layout from '../components/Layout'
+import Error from '../components/Error'
 
 const SignUp = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      lastName: '',
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      name: Yup
+        .string()
+        .trim()
+        .required('El nombre es obligatorio'),
+      lastName: Yup
+        .string()
+        .trim()
+        .required('El apellido esobligatorio'),
+      email: Yup
+        .string()
+        .trim()
+        .email('El email no es válido')
+        .required('El email es obligatorio'),
+      password: Yup
+        .string()
+        .trim()
+        .required('El password no puede ir vacio')
+        .min(6, 'El password debe ser de al menos 6 caracteres')
+    }),
+    onSubmit: _handleCreateUser
+  })
+
+  const _handleCreateUser = (user) => {
+    console.log("🚀 ~ SignUp ~ user", user)
+
+  }
+
   return (
     <Layout>
       <h1 className="text-center text-2xl text-white font-light">Sign Up</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+          >
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -19,8 +62,12 @@ const SignUp = () => {
                 id="name"
                 type="name"
                 placeholder="Nombre Usuario"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.name && formik.errors.name && <Error message={formik.errors.name} />}
 
             <div className="mb-4">
               <label
@@ -34,8 +81,12 @@ const SignUp = () => {
                 id="lastName"
                 type="lastName"
                 placeholder="Apellido Usuario"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.lastName && formik.errors.lastName && <Error message={formik.errors.lastName} />}
 
             <div className="mb-4">
               <label
@@ -49,8 +100,12 @@ const SignUp = () => {
                 id="email"
                 type="email"
                 placeholder="Email Usuario"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.email && formik.errors.email && <Error message={formik.errors.email} />}
 
             <div className="mb-4">
               <label
@@ -64,8 +119,13 @@ const SignUp = () => {
                 id="password"
                 type="password"
                 placeholder="Password Usuario"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.password && formik.errors.password && <Error message={formik.errors.password} />}
+
             <input
               type="submit"
               className="bg-gray-800 w-full mt-5 p-2 rounded text-white uppercase hover:bg-gray-900"
