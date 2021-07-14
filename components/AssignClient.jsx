51bad8538f80d20437a2ab9f5b3da92facd6ebeb
Paul Chávez <paulchavezromero20@gmail.com>
2021-07-13 21:56:46 -0500

    [update] AssignClient added

--- components/AssignClient.jsx
+++ components/AssignClient.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import Select from 'react-select'
+
+const clients = [
+  { id: 1, name: 'Paul' },
+  { id: 2, name: 'Pablo' },
+  { id: 3, name: 'Mario' }
+]
 
 const AssignClient = () => {
-  return (
-    <div>
 
-    </div>
+  const _handleChange = (...rest) => {
+    console.log("🚀 ~ NewOrder ~ rest", rest)
+  }
+
+  return (
+    <>
+      <Select
+        isMulti
+        options={clients}
+        onChange={_handleChange}
+        getOptionValue={({ id }) => id}
+        getOptionLabel={({ name }) => name}
+        placeholder="Busque o Seleccione el Cliente"
+        noOptionsMessage={() => "No hay resultados"}
+      />
+    </>
   )
 }
 
--- pages/nuevo-pedido.js
+++ pages/nuevo-pedido.js
@@ -1,32 +1,14 @@
-import Select from 'react-select'
 
+import AssignClient from '../components/AssignClient'
 import Layout from '../components/Layout'
 
-const options = [
-  { id: 'chocolate', name: 'Chocolate' },
-  { id: 'strawberry', name: 'Strawberry' },
-  { id: 'vanilla', name: 'Vanilla' }
-]
 
 const NewOrder = () => {
-
-
-  const _handleChange = (...rest) => {
-    console.log("🚀 ~ NewOrder ~ rest", rest)
-  }
 
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Crear Nuevo Pedido</h1>
-      <Select
-        isMulti
-        options={options}
-        onChange={_handleChange}
-        getOptionValue={({ id }) => id}
-        getOptionLabel={({ name }) => name}
-        placeholder="Seleccione el Producto"
-        noOptionsMessage={() => "No hay resultados"}
-      />
+      <AssignClient />
     </Layout>
   )
 }
