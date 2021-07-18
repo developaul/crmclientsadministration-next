7182bb41537bb2b5b8409daa5a5c1224c1ba470d
Paul Chávez <paulchavezromero20@gmail.com>
2021-07-17 20:31:29 -0500

    [update] bestSellers added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -32,6 +32,27 @@ const Sidebar = () => {
           </Link>
         </li>
       </nav>
+
+      <div className="sm:mt-10">
+        <p className="text-white text-2xl font-black">Otras Opciones</p>
+      </div>
+
+      <nav className="mt-5 list-none">
+        <li className={currentPathName === '/mejores-vendedores' ? 'bg-blue-800 p-2' : 'p-2'}>
+          <Link href="/mejores-vendedores">
+            <a className="text-white mb-2 block">
+              Mejores Vendedores
+            </a>
+          </Link>
+        </li>
+        <li className={currentPathName === '/mejores-clientes' ? 'bg-blue-800 p-2' : 'p-2'}>
+          <Link href="/mejores-clientes">
+            <a className="text-white mb-2 block">
+              Mejores Clientes
+            </a>
+          </Link>
+        </li>
+      </nav>
     </aside>
   )
 }
